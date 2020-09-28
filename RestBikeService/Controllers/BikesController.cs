5dbbd1478e9914b36bf18af8348da2292dd337ce
Nicolai Höyer Christiansen <nico388a@edu.easj.dk>
2020-09-28 10:49:38 +0200

    Test

--- RestBikeService/Controllers/BikesController.cs
+++ RestBikeService/Controllers/BikesController.cs
@@ -29,7 +29,7 @@ namespace RestBikeService.Controllers
         {
             return bikeList;
         }
-        //
+        
         // GET api/<BikesController>/5
         [HttpGet("{id}")]
         public Bike Get(int id)
